b384ff1419142c0fd720af46aa28d05012230245
mye <hyelim041220@naver.com>
2025-04-18 14:30:25 +0900

    초기세팅자료

---
+++ Exam_01/src/main/java/com/jslhrd/exam_01/controller/cotroller.java
@@ -0,0 +1,17 @@
+package com.jslhrd.exam_01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("")
+public class cotroller {
+
+	@GetMapping("Exam01")
+	public void Index() {
+		
+	}
+	
+}
